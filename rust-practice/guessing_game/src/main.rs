49cfdb507f7c68f669e2d92ba17d3bfb0d00bc36
阿东 <sss2363163972@gmail.com>
2025-04-29 09:50:04 +0800

    feat: 完成 guessing_game 项目

--- rust-practice/guessing_game/src/main.rs
+++ rust-practice/guessing_game/src/main.rs
@@ -1,3 +1,28 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("欢迎来到猜字谜游戏！");
+    let secret_number = rand::thread_rng().gen_range(1..100);
+    println!("字谜的答案是{secret_number}");
+    loop {
+        println!("请输入你猜的数字");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("读取失败");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("你猜的数字{}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("太小了"),
+            Ordering::Greater => println!("太大了"),
+            Ordering::Equal => {
+                println!("你赢了");
+                break;
+            }
+        }
+    }
 }
